902ab006b7e5f8a492dff797567a92a200a4990a
Kacper Goliński <kacper.golinski@netguru.co>
2015-05-19 22:51:31 +0200

    refactor code

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    delete: function(friend) {
+      var _this = this;
+
+      friend.destroy().then(function() {
+        _this.transitionTo('friends.index');
+      });
+    }
+  }
 });
