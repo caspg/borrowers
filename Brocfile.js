9cc221851fd310500c6a9ec5093d7ac763864ec0
Kacper Goliński <kacper.golinski@netguru.co>
2015-05-19 23:04:54 +0200

    include fontello

--- Brocfile.js
+++ Brocfile.js
@@ -4,6 +4,20 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
+app.import('vendor/fontello/fontello.css'); 
+app.import('vendor/fontello/font/fontello.ttf', {
+  destDir: 'font'
+});
+app.import('vendor/fontello/font/fontello.eot', { 
+  destDir: 'font'
+}); 
+app.import('vendor/fontello/font/fontello.svg', {
+  destDir: 'font'
+});
+app.import('vendor/fontello/font/fontello.woff', { 
+  destDir: 'font'
+});
+
 app.import('bower_components/picnic/releases/v2.min.css');
 
 module.exports = app.toTree();
